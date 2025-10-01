246042408725c0be7856e643acab69370cf88514
Vikas Gupta <vikasgupta0423@gmail.com>
2025-10-01 18:46:26 +0530

    add libraray react hot toast and new folder add

--- resume_frontend/src/App.jsx
+++ resume_frontend/src/App.jsx
@@ -1,13 +1,15 @@
-// import { useState } from 'react'
 
 import './App.css'
 
 function App() {
-  // const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Welcome to Resume Applicaton</h1>
+    <div className='p-10'>
+      <h1 className='text-8xl text-grey-500 font-bold'>Welcome to Resume App</h1>
+    <button className='bg-blue-500 text-white font-bold py-2 px-4 rounded'>ok</button>
+     
+    </div>
     </>
   )
 }
