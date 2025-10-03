eb9cf3fb1cc192d66df54bc63446ec3b8c6225c5
Vikas Gupta <vikasgupta0423@gmail.com>
2025-10-03 22:56:13 +0530

    Routing about home service add nav bar from daisy UI

--- resume_frontend/src/main.jsx
+++ resume_frontend/src/main.jsx
@@ -1,13 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { BrowserRouter, Route } from 'react-router-dom'
+import Root from './pages/Root'
+import Home from './pages/Home'
+import About from './pages/About'
+import Services from './pages/Services'
 import { Routes } from 'react-router'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Root />}>
-
+          <Route path="home" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="services" element={<Services />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- resume_frontend/src/pages/Root.jsx
+++ resume_frontend/src/pages/Root.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Outlet } from 'react-router'
+import Navbar from '../components/Navbar'
 
 export default function Root() {
   return (
     <div>
+      <Navbar />
       
+      <Outlet />
     </div>
   )
 }
